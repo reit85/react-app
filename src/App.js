c0bc6bedb805252d49dfebc3956979c33cd11158
Reit <Alexander.Reit@ebz-group.com>
2020-01-13 15:48:00 +0100

    AppBar hinzugefügt

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { Fragment, Component } from "react";
 import { Grid, MenuList, MenuItem } from "@material-ui/core";
+import Button from '@material-ui/core/Button';
 
-import "./App.css";
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <div className="App">
-      <nav className="App-header">
-        <MenuList>
-          <MenuItem>Übersicht</MenuItem>
-          <MenuItem>Projekte</MenuItem>
-          <MenuItem>Kunden</MenuItem>
-          <MenuItem>Mitarbeiter</MenuItem>
-          <MenuItem>Urlaubsplanung</MenuItem>
-        </MenuList>
-      </nav>
-    </div>
+    <Fragment className="App">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <NavBar />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <nav className="App-text">
+            <MenuList>
+              <MenuItem>Übersicht</MenuItem>
+              <MenuItem>Projekte</MenuItem>
+              <MenuItem>Kunden</MenuItem>
+              <MenuItem>Mitarbeiter</MenuItem>
+              <MenuItem>Urlaubsplanung</MenuItem>
+            </MenuList>
+            <Button variant="contained" color="secondary">Firat Knopf</Button>
+          </nav>
+        </Grid>
+      </Grid>
+    </Fragment>
   );
 }
 
